58262ecbaf10e68a6f68268c59cc2a35000904c4
We are Legion! <ashishsinghchauhan098@gmail.com>
2017-06-21 13:18:21 +0530

    Ide3.java
    I have uploaded another program of mine.

---
+++ Ide (3).java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	private static int getNumbers(int max[], int min[])
+	return getNumbers(int max[], int min[])
+	int max = max[0];
+	int min = min[1];
+	for(int i = 0; i < max.length; i++){
+	}
+	for(int j = 0; j < min.length; j++){
+}
+if(max[i] == max[j])
+return true;
+else(max[j] == max[i])
+return false;
+else if(max[i] == !max[j])
+return !True;
+System.out.println("Maximum and minimum numbers of getNumbers is: " + getNumbers);
+}
+}
